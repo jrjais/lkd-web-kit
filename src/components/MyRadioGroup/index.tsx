9ce9d9f7867fd2c493f3ccaae2ab69119711da33
hugo <hugosajama07@gmail.com>
2025-08-21 07:43:55 -0300

    chore: update version to 0.5.17 and refine MyRadioGroupProps interface

--- src/components/MyRadioGroup/index.tsx
+++ src/components/MyRadioGroup/index.tsx
@@ -1,26 +1,26 @@
 import { Radio, RadioGroupProps, RadioProps, Stack, Group, MantineSpacing } from '@mantine/core';
 
-export interface MyRadioGroupProps extends RadioGroupProps {
+export interface MyRadioGroupProps extends Omit<RadioGroupProps, 'children'> {
   options: RadioProps[];
   orientation?: 'horizontal' | 'vertical';
   gap?: MantineSpacing;
+  disabled?: boolean;
 }
 
 export const MyRadioGroup = ({
   options,
-  orientation = 'vertical',
+  orientation = 'horizontal',
   gap = 'xs',
   ...radioGroupProps
 }: MyRadioGroupProps) => {
   const Container = orientation === 'horizontal' ? Group : Stack;
-
   return (
     <Radio.Group {...radioGroupProps}>
       <Container gap={gap}>
         {options.map((option, index) => (
           <Radio
             key={String(option.value) || String(index)}
-            disabled={radioGroupProps.readOnly}
+            disabled={radioGroupProps.disabled || option.disabled}
             {...option}
           />
         ))}
