d1d34ab97fda651612ece23bddd0453246b627d9
hugosajama <104098663+hugosajama@users.noreply.github.com>
2026-02-23 21:51:33 -0300

    chore: update dependencies and version to 0.7.30
    feat: add FormTextarea component for form handling
    
    refactor: reorganize exports in form index file for clarity
    
    refactor: update type exports in index.ts for consistency
    
    feat: create Setter type for state management in React

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,5 @@
-export type { ApiList } from './api-list';
-export type { ApiPagination } from './api-pagination';
-export type { Setter } from './setter';
-export type { PageProps, LayoutProps } from './page-props';
-export type { DeepPartial } from './utils/deep-partial';
+export type { ApiList } from './api-list'
+export type { ApiPagination } from './api-pagination'
+export type { LayoutProps, PageProps } from './page-props'
+export type { Setter } from './setter'
+export type { DeepPartial } from './utils/deep-partial'
