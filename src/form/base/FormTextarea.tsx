01b234c9486fd4f9481a2751fcd2574269d0e4a9
jrjais <jrjais@gmail.com>
2026-03-19 22:50:55 -0300

    chore: update dependencies and version bump to 0.7.31
    - Bump version from 0.7.30 to 0.7.31 in package.json
    - Update peer dependencies for @mantine/core, @mantine/dates, @mantine/hooks, @mantine/notifications to ^8.3.18
    - Update @tanstack/react-query to ^5.91.2
    - Update next to ^16.2.0
    
    refactor: simplify FormTextarea component
    
    - Remove unnecessary imports and clean up the FormTextarea component code

--- src/form/base/FormTextarea.tsx
+++ src/form/base/FormTextarea.tsx
@@ -1,12 +1,9 @@
-import { TextareaProps } from '@mantine/core';
-import { MyTextareaProps, MyTextarea } from 'src/components';
-import { WithControllerProps, withController } from 'src/hocs';
+import { TextareaProps } from '@mantine/core'
+import { MyTextarea, MyTextareaProps } from 'src/components'
+import { WithControllerProps, withController } from 'src/hocs'
 
-export type FormTextareaProps = MyTextareaProps & WithControllerProps;
+export type FormTextareaProps = MyTextareaProps & WithControllerProps
 
 export const FormTextarea = withController<TextareaProps>(({ field, props }) => (
-  <MyTextarea
-    {...field}
-    {...props}
-  />
-));
+  <MyTextarea {...field} {...props} />
+))
